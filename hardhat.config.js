deac78ac0d04ac7a541724a94d13ad524e6e678c
Knowledge-JO <megamindtheincredible@gmail.com>
2023-07-22 10:39:31 +0100

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+require("@nomicfoundation/hardhat-toolbox");
+const dotenv = require("dotenv")
+dotenv.config()
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.18",
+  networks: {
+    mumbai:{
+      url: process.env.MUMBAI_URL || "",
+      accounts: [process.env.PRIVATE_KEY],
+      chainID: '',
+      blockConfirmations: 6
+    }
+  }
+};
